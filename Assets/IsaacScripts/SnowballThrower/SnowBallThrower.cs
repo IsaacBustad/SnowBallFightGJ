d369876a506430936e0cd9649dba989ba4fdaf5c
IsaacBustad <ijbbustad@gmail.com>
2022-12-10 20:53:31 -0600

    w
    w

--- Assets/IsaacScripts/SnowballThrower/SnowBallThrower.cs
+++ Assets/IsaacScripts/SnowballThrower/SnowBallThrower.cs
@@ -17,6 +17,8 @@ public class SnowBallThrower : MonoBehaviour
     [SerializeField] protected float throwBase = 10f;
     [SerializeField] protected float throwCurPow;
 
+    protected Camera cam;
+    protected Vector3 mPos;
 
     // methods
     protected virtual void FixedUpdate()
@@ -24,9 +26,48 @@ public class SnowBallThrower : MonoBehaviour
 
     }
 
-    protected virtual void ThrowSnow1(InputAction.CallbackContext context)
+    public virtual void Aim(InputAction.CallbackContext context)
     {
-        
+        //Vector3 worldPos = mainCam.ScreenToWorldPoint(Mouse.current.position);
+        if(cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        mPos = cam.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+
+
+    public virtual void ThrowSnow1(InputAction.CallbackContext context)
+    {
+
+        if (context.started)
+        {
+            throwCurPow = throwBase;
+        }
+        else if (context.canceled)
+        {
+
+            Vector3 rot = mPos;
+            rot.z = transform.position.z;
+
+            
+            GameObject aSB = Instantiate(snowBall, transform.position, Quaternion.identity);
+
+            
+
+            aSB.transform.LookAt(rot);
+            aSB.GetComponent<Rigidbody2D>().AddForce(transform.forward * throwCurPow, ForceMode2D.Impulse);
+
+        }
+
+            
+            
+    }
+
+    public virtual void ThrowSnow2(InputAction.CallbackContext context)
+    {
+
         if (context.started)
         {
             throwCurPow = throwBase;
@@ -37,20 +78,7 @@ public class SnowBallThrower : MonoBehaviour
         }
     }
 
-    protected virtual void ThrowSnow2(InputAction.CallbackContext context)
-    {
-
-        if (context.started)
-        {
-            throwCurPow = throwBase;
-        }
-        else if (context.canceled)
-        {
-            GameObject aSB = Instantiate(snowBall, transform.position, Quaternion.identity);
-        }
-    }
-
-    protected virtual void ThrowSnow3(InputAction.CallbackContext context)
+    public virtual void ThrowSnow3(InputAction.CallbackContext context)
     {
 
         if (context.started)
